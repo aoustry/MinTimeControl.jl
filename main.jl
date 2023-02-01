17a275611b4fe329cedb9651c097bb26929b8d11
aoustry@gmail.com <aoustry@gmail.com>
2023-02-01 17:59:27 +0100

    update

--- main.jl
+++ main.jl
@@ -14,20 +14,21 @@ MAX_NUMBER_OF_ATTEMPTS_FINAL = 10000;
 N_RANDOM_INIT = 1000
 CIRCLE_FINAL_SET = true;
 TERMINAL_CONSTRAINT_EQ = false; 
-μ = 1e-4 #1e-4 for brockett integrator #1e-4 for zermelo #1e-5 is better for toyboat
+μ = 1e-4; #1e-4 for brockett integrator #1e-4 for zermelo #5*1e-5 is better for toyboat
+ϵ = 0.1;
 STEP_ADD_TRAJ = 5
-OBJECTIVE_WITH_INTEGRAL = false;
+OBJECTIVE_WITH_INTEGRAL = false; #false
 ########################################### System definition #####################################################
 #include("system_definition_zermelo.jl")
-include("system_definition_toyboat.jl")
-#include("system_definition_brockett.jl")
+#include("system_definition_toyboat.jl")
+include("system_definition_brockett.jl")
 ##################################################################################################################
 include("basis.jl");
 include("solver.jl");
 include("plots.jl");
 ##################################################################################################################
 traj_heur,tmax = heuristic_trajectory(sys,x0,xT,Tmax)
-best_traj,tmax,λ = dual_solving(sys,x0,xT,traj_heur,tmax,0.1,μ)
+best_traj,tmax,λ = dual_solving(sys,x0,xT,traj_heur,tmax,ϵ,μ)
 plot_∇v_terminal(sys,λ,xT,tmax)
 plot_v_terminal(sys,λ,xT,tmax)
 plot_traj(traj_heur,[3,4])
