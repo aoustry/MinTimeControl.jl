e26a90ff31f81cc5a897b11363a5ed26e62cb262
aoustry@gmail.com <aoustry@gmail.com>
2023-02-01 19:02:04 +0100

    update

--- basis.jl
+++ basis.jl
@@ -4,7 +4,7 @@ using DynamicPolynomials
 
 ################################################ Begin Basis parameterization ###############################################################
 n = 1+nx;
-degree = 8; #or 9
+degree = 8; 
 @polyvar xvar[1:n]
 basis = monomials(xvar, 0:degree)
 #Polynomial basis
